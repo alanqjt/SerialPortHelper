ee3ee9e7ed3ad4cec2444f94e51b2e47efdafaaf
qujt <qujt@chinayijia.com.cn>
2019-10-14 11:44:23 +0800

    androidx  去掉

--- app/src/main/java/com/alan/serial/MainActivity.java
+++ app/src/main/java/com/alan/serial/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.TextView;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alan.serialportlib.OnSerialPortDataListener;
